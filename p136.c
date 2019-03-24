77a4e8c3053d2674bcdb8423ded5c0783a956dfb
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-03-25 02:25:12 +0530

    p136.c

---
+++ p136.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void) 
+{
+    int a[10],n,k,i,c=0;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=n;i++)
+    {
+        if(a[i]==k)  
+        {
+            c++;
+        }
+    }
+    if(c!=0)
+    {
+        printf("yes %d",c);
+    }
+    else
+    {
+        printf("no");    
+    }
+
+}
